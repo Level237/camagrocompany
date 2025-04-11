76c32acf710c554f0d7fb90aa238a54f6c3040a0
level237 <Bramslevel129@gmail.com>
2025-04-11 22:50:03 +0200

    Remove server-related files and .env configuration, update index.html for improved SEO with new meta tags and asset paths, and refactor routing in App component to utilize createBrowserRouter for better type safety.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import routes from './routes/route'
-import './index.css'
-const router = createBrowserRouter(routes);
+
 function App() {
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={routes}/>
   )
 }
 
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,34 +1,26 @@
-// src/routes/routes.tsx
-import type { RouteObject } from "react-router-dom";
-
-
+import { createBrowserRouter } from "react-router-dom";
 import Homepage from "../pages/Homepage";
-import Produits from "../pages/Produits";
-import Services from "../pages/Services";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-
-const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/produits",
-    element: <Produits />,
-  },
-  {
-    path: "/services",
-    element: <Services />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-];
+import Produits from "@/pages/Produits";
+import Services from "@/pages/Services";
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+const routes=createBrowserRouter([
+    {
+        path:"/",
+        element:<Homepage/>,
+    },{
+        path:"/produits",
+        element:<Produits/>,
+    },{
+        path:"/services",
+        element:<Services/>,
+    },{
+        path:"/about",
+        element:<About/>,
+    },{
+        path:"/contact",
+        element:<Contact/>,
+    }
+])
 
 export default routes;
